280e71dc60b0948000deb1c1df72592191ac76d3
PeterMartEsc <petermartesc@gmail.com>
2024-12-09 12:31:37 +0000

    refactor(vue.config.js): Cambios en la configuración

--- repasFormularioArcane/vue.config.js
+++ repasFormularioArcane/vue.config.js
@@ -1,4 +1,8 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/arcane-form/' // Cambia 'arcane-form' por el nombre exacto de tu repositorio
+    : '/'
+});
